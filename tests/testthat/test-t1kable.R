ac822269993af808dd5a3da0da00d15322bf40e0
Bill Denney <wdenney@humanpredictions.com>
2026-01-04 15:51:51 -0500

    Add test for latex output

--- tests/testthat/test-t1kable.R
+++ tests/testthat/test-t1kable.R
@@ -7,4 +7,8 @@ test_that("t1kable gives units without escaped HTML (#143)", {
   expect_false(
     grepl(x = t1, pattern = "&lt;", fixed = TRUE)
   )
+  t1_latex <- t1kable(x, format = "latex")
+  expect_false(
+    grepl(x = t1_latex, pattern = "&lt;", fixed = TRUE)
+  )
 })
